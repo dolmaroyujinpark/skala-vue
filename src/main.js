b1083925552c9aa3d14700e76ee4987fcccc40f1
Dora <129646253+dolmaroyujinpark@users.noreply.github.com>
2026-08-04 12:00:34 +0900

    📝  main.js 라우터 등록 주석 추가
    main.js 라우터 등록 주석 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
+// 어플리케이션 동작하기 위해 초기화하는 작업은 main.js에서 진행
 import './assets/main.css'
 
+// Router 등록하기
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './router' // 아무것도 안 써져 있으면 index.js를 자동으로 찾아서 가져옴
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
